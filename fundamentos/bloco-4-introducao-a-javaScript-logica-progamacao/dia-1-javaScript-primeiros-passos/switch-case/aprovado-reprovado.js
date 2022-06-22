ed7fba646eed458e2888d8d88ac8a16f30bdace7
ZLenon <lenoninformatica@gmail.com>
2022-06-22 00:00:08 -0300

    switch case

---
+++ fundamentos/bloco-4-introducao-a-javaScript-logica-progamacao/dia-1-javaScript-primeiros-passos/switch-case/aprovado-reprovado.js
@@ -0,0 +1,13 @@
+var lista = "reprovada";
+
+switch (lista){
+    case "aprovada":
+        console.log ("APROVADA");
+        break;
+    case "reprovada":
+        console.log ("REPROVADA");
+        break;
+    default:
+        console.log ("lista");
+        
+}
