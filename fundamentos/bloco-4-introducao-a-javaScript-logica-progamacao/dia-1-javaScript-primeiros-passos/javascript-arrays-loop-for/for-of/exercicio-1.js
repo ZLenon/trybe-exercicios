1b60ddfac26051fd527202e1b58291c50bf9b670
ZLenon <lenoninformatica@gmail.com>
2022-06-22 22:50:39 -0300

    for/of

---
+++ fundamentos/bloco-4-introducao-a-javaScript-logica-progamacao/dia-1-javaScript-primeiros-passos/javascript-arrays-loop-for/for-of/exercicio-1.js
@@ -0,0 +1,11 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+    for (let nomes of names) {
+        console.log(nomes);
+    }
+
+
+
+
+
+
+//Utilize o for/of para imprimir os elementos da lista names com o console.log():
